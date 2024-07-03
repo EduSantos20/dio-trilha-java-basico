5fdf41cc8d7ba4ec2edecba61c61992f806f9dd2
EduSantos20 <eduardosantosgt18@gmail.com>
2024-07-03 18:14:46 -0300

    segunda aula

---
+++ tipos-variaveis/src/Metodos.java
@@ -0,0 +1,10 @@
+public class Metodos {
+  public static void main(String[] args) {
+    
+  }
+
+  public double somar(int num1, int num2){
+		//LOGICA - FINALIDADE DO MÉTODO
+		return num1;
+	}
+}
